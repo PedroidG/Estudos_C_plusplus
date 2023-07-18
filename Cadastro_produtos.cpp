05c549bcac10cb900f690f666af13d3b85f0521f
Pedro Perazza <92265606+PedroidG@users.noreply.github.com>
2023-07-17 22:31:04 -0300

    adicionando escolhas ao usuário
    Adicionei um sistema básico de escolhas para o usuário escolher o que ele quer fazer.

--- Cadastro_produtos.cpp
+++ Cadastro_produtos.cpp
@@ -37,6 +37,7 @@ void busca(produto *produtos, int quantidade, string busca){
         cout << "Marca: " << produtos[posicao].marca << "\t";
         cout << "Preco: " << "R$" << produtos[posicao].preco << "\t";
     }
+    cout << endl;
 }
 
 //procedimento de ordenação
@@ -68,6 +69,7 @@ void imprimir(produto *produtos, int quantidade){
 int main(){
     int quantidade; //quantidade de produtos que serão cadastrados
     int i; //variável auxiliar
+    cout << "Digite a quantidade de produtos que deseja cadastrar: ";
     cin >> quantidade;
     cin.ignore();
     produto *produtos = new produto[quantidade]; //vetor de produtos
@@ -89,38 +91,52 @@ int main(){
     imprimir(produtos, quantidade);
     cout << endl;
 
-    //busca produtos
-    string buscador;
-    cout << "Digite o nome do produto que quer buscar: ";
-    getline(cin, buscador);
-    cout << endl;
-    busca(produtos, quantidade, buscador);
-    cout << endl;
-
-    //ordena produtos de acordo com o preço
-    ordenacao(produtos, quantidade);
-    cout << endl;
-
-    //mostra os produtos ordenados
-    cout << "Produtos ordenados de acordo com o preco: " << endl;
-    imprimir(produtos, quantidade);
-    cout << endl;
+    bool escolha = true; //variável de escolha, inicia no true para o laço while rodar de inicio
+    char opcao; //variavel que recebe a opção, o usuário digita dentro do laço while
+    while(escolha == true){
+        cout << "Escolha uma opcao: [1] Buscar produto [2] Ordenar produtos [3] Mostrar lista de produtos [4] Sair" << endl;
+        cin >> opcao;
+        cin.ignore();
+        cout << endl;
+        if(opcao == '1'){
+            //busca produtos
+            string buscador;
+            cout << "Digite o nome do produto que quer buscar: ";
+            getline(cin, buscador);
+            cout << endl;
+            busca(produtos, quantidade, buscador);
+            cout << endl;
+        }else if(opcao == '2'){
+            //ordena produtos de acordo com o preço
+            ordenacao(produtos, quantidade);
+            cout << "Produtos ordenados de acordo com o preco";
+            cout << endl;
+        }else if(opcao == '3'){
+            //mostra os produtos
+            imprimir(produtos, quantidade);
+            cout << endl;
+        }else{
+            escolha = false;
+        }
+    }
 
     //grava os produtos já ordenados em um arquivo csv com todos os campos separados corretamente em colunas
     ofstream arquivo("dados.csv");
     char separador = ';';   //char que separa cada campo em colunas no Excel
     if(arquivo.is_open()){  //verifica de o arquivo foi corretamente aberto
+        ordenacao(produtos, quantidade); //garante que os dados estarão ordenados no arquivo
         arquivo << "Produto" << separador << "Marca" << separador << "Preco" << endl; //escreve o cabeçalho
         for(i = 0; i < quantidade; i++){    //laço que escreve os dados enquanto houver produtos
             arquivo << produtos[i].nome << separador;
             arquivo << produtos[i].marca << separador;
             arquivo << "R$" << produtos[i].preco << endl;
         }
-        arquivo.close();
-        cout << "Arquivo criado com sucesso!" << endl;
+        arquivo.close();    //fecha o arquivo após receber os dados
+        cout << "Arquivo criado com sucesso!" << endl;  //mostra que o arquivo foi criado
     }else{
-        cout << "Arquivo nao foi criado!" << endl;
+        cout << "Arquivo nao foi criado!" << endl;  //mostra que houve algum erro e o arquivo não foi criado
     }
-    cout << "Programa finalizado!" << endl;
+
+    cout << "Programa finalizado!" << endl; //indica o fim do programa
     return 0;
 }
